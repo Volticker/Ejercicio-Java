8bbcc985d2eb692d8ab25f5b3c623fa362c74706
nietojuan <juandresnietog@gmail.com>
2021-10-15 11:53:01 -0300

    1ercommitjuan

---
+++ src/java_git/SoyLaComadreja.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_git;
+
+/**
+ *
+ * @author Isis
+ */
+public class SoyLaComadreja {
+    String nombre;
+    String pelaje;
+    Integer edad;
+    boolean rabia;
+}
