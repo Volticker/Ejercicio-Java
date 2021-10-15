56ef4b4d5d6f41accf33c6fe3c68bcceabedd601
Nico Fernandez <nico_fernandez5@hotmail.com>
2021-10-15 10:43:21 -0300

    Primer commit del ejercicio

---
+++ src/java_git/JAVA_GIT.java
@@ -0,0 +1,23 @@
+
+package java_git;
+
+/*
+Ahora van a continuar trabajando como mesa. Vuestra tarea ahora es que cada miembro
+de la mesa, incluido el facilitador, debe crear su branch y crear una de las siguientes
+clases: Gato, Perro, Caballo, Conejo, Pájaro y Pato. Cada uno le va a poner los atributos
+que desee.
+26
+El facilitador va a tener que crear el repositorio y subir un proyecto de Java vacio para
+que los miembros de la mesa puedan clonar y crear su clase. Una vez que cada miembro
+haya creado su clase en su respectiva rama, deberán unir todas las clases en la rama
+master, para que quede el proyecto final.
+*/
+
+public class JAVA_GIT {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
